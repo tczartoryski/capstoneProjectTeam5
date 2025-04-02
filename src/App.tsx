f6b3293e854eacce99c535859695b6772d372a22
tczartoryski <thomasczartoryski@gmail.com>
2025-04-02 15:54:14 -0400

    compressed images

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { format } from 'date-fns'; // Install date-fns for formatting timestamps
 import './styles/App.css';
 
 interface ImageData {
-    url: string;
+    thermal: string;
+    depth: string;
+    fuse: string;
+    rgb: string;
     timestamp: any; // Firestore timestamp
 }
 
@@ -23,7 +26,10 @@ const App: React.FC = () => {
         // Set up a real-time listener
         const unsubscribe = onSnapshot(imagesQuery, (snapshot) => {
             const imageData = snapshot.docs.map((doc) => ({
-                url: doc.data().url,
+                depth: doc.data().depth,
+                rgb: doc.data().rgb,
+                fuse: doc.data().fuse,
+                thermal: doc.data().thermal,
                 timestamp: doc.data().timestamp?.toDate(), // Convert Firestore timestamp to JS Date
             }));
             setImages(imageData);
@@ -63,33 +69,129 @@ const App: React.FC = () => {
                                 backgroundColor: '#fff', // Add a background color for better contrast
                             }}
                         >
-                            <div
-                                style={{
-                                    borderRadius: '16px', // Rounded corners for the image container
-                                    overflow: 'hidden', // Ensure the image respects the border radius
-                                }}
-                            >
-                                <img
-                                    src={image.url}
-                                    alt={`Image ${index + 1}`}
-                                    style={{
-                                        width: '100%', // Ensure the image fits the container width
-                                        height: 'auto', // Maintain aspect ratio
-                                        maxHeight: '400px', // Optional: Limit the height of the image
-                                        objectFit: 'contain', // Ensure the image fits nicely within the container
-                                    }}
-                                />
-                            </div>
                             <Typography
-                                variant="body2"
-                                color="textSecondary"
+                                variant="h6"
                                 style={{
-                                    marginTop: '10px',
+                                    marginBottom: '10px',
                                     textAlign: 'center',
                                 }}
                             >
                                 Uploaded on: {image.timestamp ? format(image.timestamp, 'HH:mm:ss') : 'Unknown'}
                             </Typography>
+                            <Box
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    gap: '20px', // Space between the two boxes
+                                    alignItems: 'center', // Align the boxes vertically
+                                }}
+                            >
+                                {/* Box with Three Images */}
+                                <Box
+                                    style={{
+                                        display: 'flex',
+                                        flex: 3, // Take more space for the three images
+                                        justifyContent: 'space-between',
+                                        gap: '10px',
+                                    }}
+                                >
+                                    {/* Thermal Image */}
+                                    <div
+                                        style={{
+                                            flex: 1,
+                                            borderRadius: '16px',
+                                            overflow: 'hidden',
+                                        }}
+                                    >
+                                        <Typography variant="subtitle1" style={{ marginBottom: '5px' }}>
+                                            Thermal
+                                        </Typography>
+                                        <img
+                                            src={image.thermal}
+                                            alt={`Thermal Image ${index + 1}`}
+                                            style={{
+                                                width: '100%',
+                                                height: 'auto',
+                                                maxHeight: '200px',
+                                                objectFit: 'contain',
+                                            }}
+                                        />
+                                    </div>
+
+                                    {/* RGB Image */}
+                                    <div
+                                        style={{
+                                            flex: 1,
+                                            borderRadius: '16px',
+                                            overflow: 'hidden',
+                                        }}
+                                    >
+                                        <Typography variant="subtitle1" style={{ marginBottom: '5px' }}>
+                                            Depth
+                                        </Typography>
+                                        <img
+                                            src={image.depth}
+                                            alt={`RGB Image ${index + 1}`}
+                                            style={{
+                                                width: '100%',
+                                                height: 'auto',
+                                                maxHeight: '200px',
+                                                objectFit: 'contain',
+                                            }}
+                                        />
+                                    </div>
+
+                                    {/* Depth Image */}
+                                    <div
+                                        style={{
+                                            flex: 1,
+                                            borderRadius: '16px',
+                                            overflow: 'hidden',
+                                        }}
+                                    >
+                                        <Typography variant="subtitle1" style={{ marginBottom: '5px' }}>
+                                            RGB
+                                        </Typography>
+                                        <img
+                                            src={image.rgb}
+                                            alt={`Depth Image ${index + 1}`}
+                                            style={{
+                                                width: '100%',
+                                                height: 'auto',
+                                                maxHeight: '200px',
+                                                objectFit: 'contain',
+                                            }}
+                                        />
+                                    </div>
+                                </Box>
+
+                                {/* Box with Fuse Image */}
+                                <Box
+                                    style={{
+                                        flex: 1, // Take less space for the fuse image
+                                        borderRadius: '16px',
+                                        overflow: 'hidden',
+                                        boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)', // Add shadow for better contrast
+                                        padding: '10px',
+                                        backgroundColor: '#fff', // Add background color
+                                    }}
+                                >
+                                    <Typography variant="subtitle1" style={{ marginBottom: '5px' }}>
+                                            Fused
+                                    </Typography>
+                                    <img
+                                        src={image.fuse}
+                                        alt={`Fuse Image ${index + 1}`}
+                                        style={{
+                                            width: '100%',
+                                            height: 'auto',
+                                            maxHeight: '200px',
+                                            objectFit: 'contain',
+                                            borderRadius: '16px',
+                                        }}
+                                    />
+                                </Box>
+                            </Box>
                         </Box>
                     ))}
                 </Box>
